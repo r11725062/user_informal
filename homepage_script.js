35bbf2f6b8201e6d57e5181cf850f187041a3227
r11725062 <163897811+r11725062@users.noreply.github.com>
2024-03-22 18:15:37 +0800

    Update homepage_script.js

--- homepage_script.js
+++ homepage_script.js
@@ -1,30 +1,7 @@
-let currentPageIndex = 0;
-const pages = document.querySelectorAll('.page');
 const nextButton = document.getElementById('next');
-const prevButton = document.getElementById('prev');
 
-function updateButtons() {
-    prevButton.style.display = currentPageIndex === 0 ? 'none' : 'inline-block';
-    nextButton.textContent = currentPageIndex === pages.length - 1 ? 'START' : 'NEXT';
-}
 
 nextButton.addEventListener('click', function() {
-    if (currentPageIndex < pages.length - 2) {
-        pages[currentPageIndex].classList.remove('active');
-        pages[++currentPageIndex].classList.add('active');
-        updateButtons();
-    } else {
-        window.location.href = 'avatar.html'; // 替換成你的跳轉目標頁面
-    }
+    window.location.href = 'avatar.html'; // 替換成你的跳轉目標頁面
 });
 
-prevButton.addEventListener('click', function() {
-    if (currentPageIndex > 0) {
-        pages[currentPageIndex].classList.remove('active');
-        pages[--currentPageIndex].classList.add('active');
-        updateButtons();
-    }
-});
-
-
-updateButtons(); // 初始化按鈕狀態
